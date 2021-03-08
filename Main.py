5ac4bc6127fca65a876275ca035a7cf826708965
oilyshelf <Rost-Iskan@web.de>
2021-03-08 17:12:25 +0100

    Update Main.py
    started with the Api , imported packages
    
    Co-Authored-By: ScarFace06 <62728015+ScarFace06@users.noreply.github.com>

--- Main.py
+++ Main.py
@@ -1,1 +1,31 @@
-print("test lol")
+from flask import Flask,request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'Hallo': 'du Hurensohn'}
+
+
+class webhook(Resource):
+    def post(self):
+        some_json = request.get_json()
+        print(some_json)
+        response = {'fulfillmentText': 'This is a response from webhook.'}
+        if some_json['queryResult']['action'] == 'zutaten.selector' :
+            if some_json['queryResult']['parameters']['ingredients'] == 'Tomaten':
+                response = {'fulfillmentText': 'schlechte wahl'}
+            else:
+                response = {'fulfillmentText': 'gute wahl'}
+
+
+
+        return response, 201
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(webhook,'/webhook')
+
+if __name__ == '__main__':
+    app.run(debug=True)
