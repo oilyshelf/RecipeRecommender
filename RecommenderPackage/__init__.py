c6a8d6fe5b24d6a5e26734558727e9bfecc779b8
Rosti <rostislav.iskandirov@student.uni-siegen.de>
2021-03-09 20:56:25 +0100

    build a Framework for the classes

---
+++ RecommenderPackage/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["recommender"]
+
+from .recommender import Recommender
---
+++ RecommenderPackage/recommender.py
@@ -0,0 +1,18 @@
+
+class Recommender():
+    def __init__(self):
+        #TODO init the recommender
+        return
+
+    def knowledge_recom(self):
+        #TODO 
+        return
+
+    def contend_recom(self):
+        #TODO 
+        return
+
+    def create_userProfile(self):
+        #TODO
+        return
+        
